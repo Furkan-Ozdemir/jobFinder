87e875c574c5d519ee686a95f2880883572b4a3d
Furkan Özdemir <74561643+Furkan-Ozdemir@users.noreply.github.com>
2024-12-20 15:27:09 +0300

    Refactor Main and Header components for improved layout and styling

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,16 +4,23 @@ import "./index.scss";
 export default function Main() {
   return (
     <main className="main">
-      <div>
-        <p className="main__title">
-          <span>Over</span>
-          <span className="main__title__highlight"> 5,000 jobs </span>
-          <span>are waiting for you</span>
-        </p>
-        <p>Work with the best companies, hire the experienced professionals</p>
-      </div>
-      <div className="main__form">
-        <InlineForm />
+      <div className="main__container">
+        <div>
+          <p className="main__container__title">
+            <span>Over</span>
+            <span className="main__container__title__highlight">
+              {" "}
+              5,000 jobs{" "}
+            </span>
+            <span>are waiting for you</span>
+          </p>
+          <p>
+            Work with the best companies, hire the experienced professionals
+          </p>
+        </div>
+        <div className="main__form">
+          <InlineForm />
+        </div>
       </div>
     </main>
   );
