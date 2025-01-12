89fd0dfcdf78cfbbccd83232b7d956452de917da
Furkan Özdemir <74561643+Furkan-Ozdemir@users.noreply.github.com>
2025-01-12 23:52:31 +0300

    redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,34 @@
 import { createRoot } from "react-dom/client";
-
 import "./index.css";
-
 import AppRoutes from "./AppRoutes.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer, Zoom } from "react-toastify";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <AppRoutes />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        draggable
-        theme="dark"
-        transition={Zoom}
-        style={{ fontSize: "1.4rem", fontFamily: "Montserrat" }}
-      />
-    </QueryClientProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          theme="dark"
+          transition={Zoom}
+          style={{ fontSize: "1.4rem", fontFamily: "Montserrat" }}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover={false}
+        />
+      </QueryClientProvider>
+    </PersistGate>
   </Provider>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // only auth will be persisted
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
